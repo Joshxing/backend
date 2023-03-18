3f2a91d1bca5b315e9b26fa51656c64ec703a2cc
Josh <53200365+Joshxing@users.noreply.github.com>
2023-03-19 00:56:57 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,33 +3,13 @@ import fs from 'fs';
 import path from 'path';
 
 
-
-// const url = import.meta.url;
-// const split = url.split('/');
-// const directory = split.slice(3, split.length - 1).join('/');
-// let newPath = path.join(directory,'ca-certificate.crt').replace('%20', ' ');
-
-// const ca = fs.readFileSync(newPath);
-
 // create the connection to database
-// const db = mysql.createConnection({
-//     host: '',
-//     user: 'doadmin',
-//     password: '',
-//     database: 'db_restaurant',
-//     port: '25060',
-//     ssl: {
-//       ca: cert,
-//     }
-// });
 
-// create the connection to database
 const db = mysql.createConnection({
-    host: process.env.DB_DOMAIN,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_DEFAULT_SCHEMA,
-    port: process.env.DB_PORT,
+    host: "localhost/8081",
+    user: "root",
+    password: "",
+    database: "db_restaurant"
 });
 
 
