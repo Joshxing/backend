caf7627067c1bb54acf6dc1e4de749a723d867ce
Joshua <kerzjhon1@Gmail.com>
2023-03-19 00:12:10 +0800

    commmit

---
+++ routes/routes.js
@@ -0,0 +1,130 @@
+// import express 
+import express from "express";
+// import functions from controller 
+import {
+    showFood,
+    showFoodById,
+    createFood,
+    updateFood,
+    deleteFood,
+} from "../controllers/food.js";
+
+import {
+    showAUser,
+    createAccount
+} from "../controllers/user.js";
+
+import {
+    addItems,
+    getItem,
+    updateItem,
+    allItems,
+    deleteItem,
+    deleteItems
+} from "../controllers/cart.js";
+
+import {
+    createBooking
+} from "../controllers/booktable.js";
+
+import {
+    createBillDetails,getBillDetailsById
+} from "../controllers/billdetails.js";
+
+import {
+    showNewestStatusId,
+    createBillStatus, 
+    getAllBillsByUser,
+    getAllBillsByBill,
+    getAllBills,
+    updateBillStatus,
+    updateBillPaid,
+    cancelBillStatus
+} from "../controllers/billstatus.js";
+
+// init express router
+const router = express.Router();
+
+////////////////////////// FOOD ////////////////////////////////
+// get all Food
+router.get("/api/food", showFood);
+
+// get single Food 
+router.get("/api/food/:id", showFoodById);
+
+// create Food
+router.post("/api/food", createFood);
+
+// update Food 
+router.put("/api/food/:id", updateFood);
+
+// delete Food
+router.delete("/api/food/:id", deleteFood);
+
+
+
+////////////////////////// USER ////////////////////////////////
+// get all user
+router.get("/api/users/:email", showAUser);
+
+// create account
+router.post("/api/users/", createAccount);
+
+
+
+////////////////////////// CART ////////////////////////////////
+// add to cart
+router.post("/api/cartItem", addItems);
+
+// get a item in cart
+router.get("/api/cartItem/:user_id/:food_id", getItem);
+
+// get all items by user id
+router.get("/api/cartItem/:id", allItems);
+
+// update item qty
+router.put("/api/cartItem/", updateItem);
+
+// delete a item in cart
+router.delete("/api/cartItem/:user_id/:food_id", deleteItem);
+
+// delete all items in cart
+router.delete("/api/cartItem/:id", deleteItems);
+
+
+
+////////////////////////// Booking ////////////////////////////////
+router.post("/api/booking", createBooking);
+
+
+
+////////////////////////// Bill Details ////////////////////////////////
+router.post("/api/billdetails", createBillDetails);
+router.get("/api/billdetails/:id", getBillDetailsById);
+
+
+
+////////////////////////// Bill Status ////////////////////////////////
+router.get("/api/billstatus/new", showNewestStatusId);
+router.post("/api/billstatus", createBillStatus);
+router.get("/api/billstatus/user/:id", getAllBillsByUser);
+router.get("/api/billstatus/bill/:id", getAllBillsByBill);
+router.get("/api/billstatus", getAllBills);
+router.put("/api/billstatus/:id", updateBillStatus);
+router.put("/api/billstatus/paid/:id", updateBillPaid);
+router.put("/api/billstatus/cancel/:id", cancelBillStatus);
+
+
+
+
+
+// export default router
+export default router;
+
+
+
+
+
+
+
+
